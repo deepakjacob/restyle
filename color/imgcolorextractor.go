dabbf677e394aee98891bb606dff49354bce5830
Deepak Jacob <deepak.jacob@gmail.com>
2019-07-29 23:44:30 -0400

    tests: for image color extraction

--- color/imgcolorextractor.go
+++ color/imgcolorextractor.go
@@ -3,25 +3,27 @@ package color
 import (
 	"fmt"
 	"image"
+	_ "image/jpeg" // need for jpg processing
 	"io"
 
+	"github.com/deepakjacob/restyle/logger"
 	"github.com/generaltso/vibrant"
-)
-
-var (
-	errColorExtraction = fmt.Errorf("error extracting colors from image")
+	"go.uber.org/zap"
 )
 
 // Color extracts color from the provided image
 func Color(f io.Reader) error {
 	img, _, err := image.Decode(f)
 	if err != nil {
-		return errColorExtraction
+		logger.Log.Error("image decoding failed", zap.Error(err))
+		return err
 	}
 
-	palette, err := vibrant.NewPaletteFromImage(img)
+	// palette, err := vibrant.NewPaletteFromImage(img)
+	palette, err := vibrant.NewPalette(img, 10)
 	if err != nil {
-		return errColorExtraction
+		logger.Log.Error("error in creating palette", zap.Error(err))
+		return err
 	}
 
 	for name, swatch := range palette.ExtractAwesome() {
---
+++ color/imgcolorextractor_test.go
@@ -0,0 +1,26 @@
+package color
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/deepakjacob/restyle/logger"
+)
+
+func TestColor(t *testing.T) {
+	if err := logger.Init(-1, ""); err != nil {
+		log.Fatal("logger initialization failed for tests")
+	}
+	path, _ := os.Getwd()
+	path += "/test_data/saree2.jpg"
+	img, err := os.Open(path)
+	if err != nil {
+		t.Error("error in opening file", err)
+	}
+	defer img.Close()
+	err = Color(img)
+	if err != nil {
+		t.Errorf("error in processing file %v", err)
+	}
+}
